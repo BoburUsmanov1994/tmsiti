4140a7bc7fb7c340017822682c3bdc667c82b185
Bobur <boburusmanov1994@gmail.com>
2023-04-04 13:38:55 +0500

    feat:"material volumes"

--- src/components/select/index.jsx
+++ src/components/select/index.jsx
@@ -1,5 +1,45 @@
 import React, {useEffect, useState} from 'react';
-import Select from 'react-select';
+import RSelect,{components}  from 'react-select';
+import Image from "next/image";
+const DropdownIndicator = props => {
+    return (
+        components.DropdownIndicator && (
+            <components.DropdownIndicator {...props}>
+                <Image width={12} height={6} src={'/icons/arrow-down-dark'} alt={'arrow'} />
+            </components.DropdownIndicator>
+        )
+    );
+};
+const customStyles = {
+    control: (base, state, error) => ({
+        ...base,
+        background: "#fff",
+        borderColor:  "transparent",
+        borderRadius: '5px',
+        outline: "none",
+        boxShadow: "none",
+        color: "#7E7E7E",
+        display: "flex",
+        overflow: 'hidden',
+        padding: '4px 12px',
+        width: '100%',
+        minHeight: '40px',
+        fontSize: '14px',
+        fontWeight: '400',
+        "&:hover": {
+            borderColor: '#202B57',
+            outline: "none",
+        },
+        "&:focus": {
+            borderColor: '#13D6D1',
+            outline: "none",
+        }
+    }),
+    indicatorSeparator: (base, state) => ({
+        ...base,
+        display: 'none'
+    })
+};
 const Select = ({label='',getValue=()=>{},options=[]}) => {
     const [selectedOption,setSelectedOption] = useState();
     useEffect(()=>{
@@ -7,7 +47,11 @@ const Select = ({label='',getValue=()=>{},options=[]}) => {
     },[selectedOption])
     return (
         <div>
-            <Select
+            <label className={'mb-2 inline-block'} htmlFor={label}>{label}</label>
+            <RSelect
+                clearIndicator={true}
+                styles={customStyles}
+                id={label}
                 defaultValue={selectedOption}
                 onChange={setSelectedOption}
                 options={options}
--- src/pages/materials/volume/[id]/index.jsx
+++ src/pages/materials/volume/[id]/index.jsx
@@ -3,6 +3,7 @@ import Main from "@/layouts/main";
 import Section from "@/components/section";
 import Menu from "@/components/menu";
 import Title from "@/components/title";
+import Select from "@/components/select";
 
 const Index = () => {
     return (
@@ -12,6 +13,18 @@ const Index = () => {
                 <div className="grid grid-cols-12">
                     <div className="col-span-12 text-center mt-5">
                         <Title  center>Materiallar va buyumlar</Title>
+                    </div>
+                    <div className="col-span-12 mb-5">
+                        <Select label={'Tanlangan mahsulot turi'} />
+                    </div>
+                    <div className="col-span-12 mb-5">
+                        <Select label={'Tanlangan bo‘lim'} />
+                    </div>
+                    <div className="col-span-12 mb-5">
+                        <Select label={'Tanlangan kategoriya'} />
+                    </div>
+                    <div className="col-span-12 mb-5">
+                        <Select label={'Tanlangan guruh'} />
                     </div>
                 </div>
             </Section>
