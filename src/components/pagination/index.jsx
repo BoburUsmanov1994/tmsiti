dbc779d882097a225a3d2c7fbe4a55f048371eb2
Bobur <boburusmanov1994@gmail.com>
2023-04-05 15:58:59 +0500

    fix:"filter"

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -10,8 +10,8 @@ const Pagination = ({
     return (
         <div>
             <ReactPaginate forcePage={page - 1} onPageChange={({selected}) => setPage(selected + 1)}
-                           pageCount={pageCount} nextLabel={<span>Oldingi</span>}
-                           previousLabel={<span>Keyingi</span>} className={'flex justify-center my-2 items-center'}
+                           pageCount={pageCount} nextLabel={<span>Keyingi</span>}
+                           previousLabel={<span>Oldingi</span>} className={'flex justify-center my-2 items-center'}
                            pageClassName={'ml-2.5'}
                            pageLinkClassName={'inline-flex w-8 h-8 rounded-[5px] justify-center items-center bg-[#E0E0E0] text-black text-sm'}
                            nextClassName={'ml-2.5  text-sm text-[#313B3D]'} previousClassName={'text-sm text-[#313B3D]'}
