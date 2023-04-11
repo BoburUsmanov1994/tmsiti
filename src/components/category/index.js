f5579c2c73ac9d68ee0cd391a1fa2853e8a29c94
Bobur <boburusmanov1994@gmail.com>
2023-04-11 11:37:54 +0500

    fix:"image"

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Image from "next/image";
 import {get} from "lodash";
-import {config} from "@/config";
 import Link from "next/link";
 
-const Category = ({data,url='materials/volume',name='volume_name'}) => {
+const Category = ({data,url='materials/volume',name='volume_name',logo_url='volume_logo'}) => {
     return (
         <Link href={`/${url}/${get(data,'id')}`} className={'rounded-[5px] p-2.5 bg-white drop-shadow-category flex items-start min-h-full'}>
-            <Image height={38} width={38} src={`${config.FILE_URL}${get(data,'id')}.png`}  loader={() => `${config.FILE_URL}${get(data,'id')}.png`} />
+            <Image height={38} width={38} src={get(data,logo_url)}  loader={() => get(data,logo_url)} />
             <h4 className={'ml-3 text-sm'}>{get(data,name)}</h4>
         </Link>
     );
