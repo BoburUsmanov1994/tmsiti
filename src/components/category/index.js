1b3a4fceeb894f5067d42ec97adc48e55279e619
BoburUsmanoff <bobur@pixeel.co.uk>
2023-04-11 05:03:43 +0500

    "fix"

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Category = ({data,url='materials/volume',name='volume_name'}) => {
     return (
-        <Link href={`/${url}/${get(data,'id')}`} className={'rounded-[5px] p-2.5 bg-white drop-shadow-category flex'}>
+        <Link href={`/${url}/${get(data,'id')}`} className={'rounded-[5px] p-2.5 bg-white drop-shadow-category flex items-start min-h-full'}>
             <Image height={38} width={38} src={`${config.FILE_URL}${get(data,'id')}.png`}  loader={() => `${config.FILE_URL}${get(data,'id')}.png`} />
             <h4 className={'ml-3 text-sm'}>{get(data,name)}</h4>
         </Link>
