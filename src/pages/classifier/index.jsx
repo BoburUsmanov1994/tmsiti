6aeacdb8eec140274808a2dcd8571c5882fa4c26
BoburUsmanoff <bobur@pixeel.co.uk>
2023-04-23 15:45:34 +0500

    fix:resources

--- src/pages/classifier/index.jsx
+++ src/pages/classifier/index.jsx
@@ -68,7 +68,7 @@ const columns = [
     }
 ]
 const Classifier = () => {
-    const [count,setCount] = useState(0)
+    const [count, setCount] = useState(0)
     const [search, setSearch] = useState('')
     const [volumeId, setVolumeId] = useState(null)
     const [partId, setPartId] = useState(null)
@@ -81,7 +81,7 @@ const Classifier = () => {
             key: "volumes"
         }
     });
-    const {data: parts, isLoading: isLoadingParts,isFetching:isFetchingParts} = useGetQuery({
+    const {data: parts, isLoading: isLoadingParts, isFetching: isFetchingParts} = useGetQuery({
         key: [KEYS.classifier, volumeId],
         url: URLS.classifier,
         params: {
@@ -90,13 +90,31 @@ const Classifier = () => {
         },
         enabled: !!(volumeId)
     });
+    const {data: chapters, isLoading: isLoadingChapters, isFetching: isFetchingChapters} = useGetQuery({
+        key: [KEYS.classifier, volumeId, partId],
+        url: URLS.classifier,
+        params: {
+            key: "chapters",
+            parent: partId
+        },
+        enabled: !!(partId)
+    });
+    const {data: groups, isLoading: isLoadingGroups, isFetching: isFetchingGroups} = useGetQuery({
+        key: [KEYS.classifier, volumeId, partId,chapterId],
+        url: URLS.classifier,
+        params: {
+            key: "groups",
+            parent: chapterId
+        },
+        enabled: !!(chapterId)
+    });
 
 
     if (isLoadingVolumes) {
         return <Main><ContentLoader/></Main>;
     }
 
-    console.log('parts',parts)
+    console.log('partId', partId)
     return (
         <Main>
             <Menu active={6}/>
@@ -111,7 +129,10 @@ const Classifier = () => {
                                 <label className={' font-medium text-primary'} htmlFor="#">
                                     Qidiruv
                                 </label>
-                                <p className={'text-sm text-[#516164]'}>*<NumericFormat value={count} displayType={'text'} thousandSeparator={" "} /> natija topildi</p>
+                                <p className={'text-sm text-[#516164]'}>*<NumericFormat value={count}
+                                                                                        displayType={'text'}
+                                                                                        thousandSeparator={" "}/> natija
+                                    topildi</p>
                             </div>
                             <input onChange={debounce(function (e) {
                                     setSearch(e.target.value)
@@ -119,37 +140,104 @@ const Classifier = () => {
                             )} placeholder={'Kerakli mahsulot nomini yozing'}
                                    className={'border border-transparent w-full px-5 py-2.5 rounded-[5px] bg-white placeholder:italic placeholder:text-[#516164] h-[46px] rounded-[5px] outline-none focus:border-[#28366D]'}
                                    type="text"/>
-                            {search && search.length < 4 && <span className={'text-red-500 text-xs font-light'}>Kamida 4 ta belgi kiriting</span>}
+                            {search && search.length < 4 &&
+                            <span className={'text-red-500 text-xs font-light'}>Kamida 4 ta belgi kiriting</span>}
                         </div>
                     </div>
                     <div className="col-span-3">
                         <ul className={'bg-white shadow-category p-2'}>
                             {
-                                get(volumes, 'data.results', []).map(volume => <li
-                                    onClick={() => setVolumeId(get(volume, 'id'))}
-                                    className={clsx('p-1.5 transition cursor-pointer mb-3 hover:bg-[#C7E3FC]', {'text-[#1B41C6] font-medium hover:bg-transparent': get(volume, 'id') == volumeId})}
+                                get(volumes, 'data.results', []).map((volume,i) => <li
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        setGroupId(null);
+                                        setChapterId(null);
+                                        setPartId(null);
+                                        setVolumeId(get(volume, 'id'))
+                                    }}
+                                    className={clsx('p-1.5 transition cursor-pointer mb-3 hover:bg-[#C7E3FC]', {'text-[#1B41C6] font-medium hover:bg-transparent': get(volume, 'id') == volumeId,'!mb-0':get(volumes, 'data.results', [])?.length == i+1})}
                                     key={get(volume, 'id')}>
                                     <div className={'flex items-start'}>
-                                    <motion.div className={'mr-2 flex-none'} animate={{
-                                        rotate: get(volume, 'id') == volumeId ? 180 : 0,
-                                    }}><Image width={24} height={24}
-                                              src={get(volume, 'id') == volumeId ? '/icons/arrow-down-category-active.svg' : '/icons/arrow-down-category.svg'}
-                                              alt={'arrow'}/>
-                                    </motion.div>
-                                    <span>{get(volume, 'volume_name')}</span>
+                                        <motion.div className={'mr-2 flex-none'} animate={{
+                                            rotate: get(volume, 'id') == volumeId ? 180 : 0,
+                                        }}><Image width={24} height={24}
+                                                  src={get(volume, 'id') == volumeId ? '/icons/arrow-down-category-active.svg' : '/icons/arrow-down-category.svg'}
+                                                  alt={'arrow'}/>
+                                        </motion.div>
+                                        <span>{get(volume, 'volume_name')}</span>
                                     </div>
-                                    {get(volume, 'id') == volumeId && ((isLoadingParts || isFetchingParts) ? <ContentLoader classNames={'!bg-transparent min-h-[25vh]'} /> : <ul className={'pl-3 py-1.5'}>
-                                        {get(parts,'data.results',[]).map((part,j)=><li  onClick={() => setPartId(get(part, 'id'))}
-                                                                                     className={clsx(' transition cursor-pointer mb-2 flex items-start hover:text-[#1B41C6] text-sm text-[#28366D] font-normal', {'text-[#017EFA] !font-medium': get(part, 'id') == partId,'mb-0':get(parts,'data.results',[])?.length == j+1})} key={get(part,'id')}>
-                                            <motion.div className={'mr-2 flex-none '} animate={{
-                                                rotate: get(part, 'id') == partId ? 90 : 0,
-                                            }}><Image width={18} height={18}
-                                                      src={get(part, 'id') == partId ? '/icons/arrow-down-category-active.svg' : '/icons/arrow-down-category.svg'}
-                                                      alt={'arrow'}/>
-                                            </motion.div>
-                                            <span>{get(part, 'part_name')}</span>
-                                        </li>)}
-                                    </ul>)}
+                                    {get(volume, 'id') == volumeId && ((isLoadingParts || isFetchingParts) ?
+                                        <ContentLoader classNames={'!bg-transparent min-h-[25vh]'}/> :
+                                        <ul className={'pl-3 py-1.5'}>
+                                            {get(parts, 'data.results', []).map((part, j) => <li onClick={(e) => {
+                                                e.stopPropagation();
+                                                setGroupId(null);
+                                                setChapterId(null);
+                                                setPartId(get(part, 'id'));
+                                            }}
+                                                                                                 className={clsx(' transition cursor-pointer mb-2  hover:text-[#1B41C6] text-sm text-[#28366D] font-normal', {
+                                                                                                     'text-[#017EFA] !font-medium': get(part, 'id') == partId,
+                                                                                                     'mb-0': get(parts, 'data.results', [])?.length == j + 1
+                                                                                                 })}
+                                                                                                 key={get(part, 'id')}>
+                                                <div className={'flex items-start'}>
+                                                    <motion.div className={'mr-2 flex-none '} animate={{
+                                                        rotate: get(part, 'id') == partId ? 90 : 0,
+                                                    }}><Image width={18} height={18}
+                                                              src={get(part, 'id') == partId ? '/icons/arrow-down-category-active.svg' : '/icons/arrow-down-category.svg'}
+                                                              alt={'arrow'}/>
+                                                    </motion.div>
+                                                    <span>{get(part, 'part_name')}</span>
+                                                </div>
+                                                {get(part, 'id') == partId && ((isLoadingChapters || isFetchingChapters) ?
+                                                    <ContentLoader classNames={'!bg-transparent min-h-[25vh]'}/> :
+                                                    <ul className={'pl-5 py-1.5'}>
+                                                        {
+                                                            get(chapters, 'data.results', []).map((chapter, k) => <li
+                                                                className={clsx(' transition cursor-pointer mb-1.5  hover:text-[#1B41C6] text-sm text-[#28366D] font-normal', {
+                                                                    'text-[#017EFA] !font-medium': get(chapter, 'id') == chapterId,
+                                                                    'mb-0': get(chapters, 'data.results', [])?.length == k + 1
+                                                                })}
+                                                                key={get(chapter, 'id')}
+                                                                onClick={(e) => {
+                                                                    e.stopPropagation();
+                                                                    setGroupId(null);
+                                                                    setChapterId(get(chapter, 'id'));
+                                                                }}
+                                                            >
+                                                                <div className={'flex items-start'}>
+                                                                    <motion.div className={'mr-2 flex-none '} animate={{
+                                                                        rotate: get(chapter, 'id') == chapterId ? 90 : 0,
+                                                                    }}><Image width={18} height={18}
+                                                                              src={get(chapter, 'id') == chapterId ? '/icons/arrow-down-category-active.svg' : '/icons/arrow-down-category.svg'}
+                                                                              alt={'arrow'}/>
+                                                                    </motion.div>
+                                                                    <span>{get(chapter, 'chapter_name')}</span>
+                                                                </div>
+                                                                {get(chapter, 'id') == chapterId && ((isLoadingGroups || isFetchingGroups) ?
+                                                                    <ContentLoader classNames={'!bg-transparent min-h-[25vh]'}/> : <ul className={'pl-9 py-1.5'}>
+                                                                        {
+                                                                            get(groups, 'data.results', []).map((group, l) => <li
+                                                                            key={get(group,'id')}
+                                                                            className={clsx(' transition cursor-pointer mb-1.5  hover:text-[#1B41C6] text-sm text-[#28366D] font-normal', {
+                                                                                'text-[#017EFA] !font-medium': get(group, 'id') == groupId,
+                                                                                'mb-0': get(groups, 'data.results', [])?.length == l + 1
+                                                                            })}
+                                                                            onClick={(e) => {
+                                                                                e.stopPropagation();
+                                                                                setGroupId(get(group, 'id'));
+                                                                            }}
+                                                                            >
+                                                                                {get(group,'group_name')}
+                                                                            </li>)
+                                                                        }
+
+                                                                    </ul>)}
+                                                            </li>)
+                                                        }
+                                                    </ul>)}
+                                            </li>)}
+                                        </ul>)}
                                 </li>)
                             }
 
@@ -169,6 +257,7 @@ const Classifier = () => {
                                 </div>}
                                         defaultValue={getDefaultValue(getOptionList(get(volumes, 'data.results', []), 'id', 'volume_name'), volumeId)}
                                         getValue={(val) => {
+                                            setGroupId(null);
                                             setVolumeId(get(val, 'value'))
                                         }}
                                         options={getOptionList(get(volumes, 'data.results', []), 'id', 'volume_name')}
@@ -179,24 +268,40 @@ const Classifier = () => {
                                     <label className={' font-medium text-primary'} htmlFor="#">
                                         Tanlangan Guruh
                                     </label>
-                                    <p className={'text-sm text-[#516164]'}>*3568 natija topildi</p>
-                                </div>} options={[]} placeholder={'Barcha guruhlar'}/>
+                                    <p className={'text-sm text-[#516164]'}>*<NumericFormat displayType={'text'}
+                                                                                            thousandSeparator={" "}
+                                                                                            value={get(groups, 'data.count', 0)}/> natija topildi</p>
+                                </div>}
+                                        defaultValue={getDefaultValue(getOptionList(get(groups, 'data.results', []), 'id', 'group_name'), groupId)}
+                                        getValue={(val) => {
+                                            setGroupId(get(val, 'value'))
+                                        }}
+                                        options={getOptionList(get(groups, 'data.results', []), 'id', 'group_name')} placeholder={'Barcha guruhlar'}/>
                             </div>
                             <div className="col-span-12 mt-5">
-                                {volumeId ?  <GridView
+                                {volumeId ? <GridView
                                         getCount={setCount}
                                         url={URLS.classifierResources}
-                                        key={[KEYS.classifierResources,volumeId]}
-                                        params={search && search?.length > 3 ? {key: 'name', parent: search} : {key: 'volume',value:volumeId}}
+                                        key={[KEYS.classifierResources, volumeId]}
+                                        params={search && search?.length > 3 ? {
+                                            key: 'name',
+                                            value: search
+                                        } : {
+                                            key: groupId ? 'group' : chapterId ? 'chapter' : partId ? 'part' : 'volume',
+                                            value: groupId ? groupId : chapterId ? chapterId : partId ? partId : volumeId
+                                        }}
                                         columns={columns}
-                                    />:
-                                <GridView
-                                    getCount={setCount}
-                                    url={URLS.classifier}
-                                    key={KEYS.classifier}
-                                    params={search && search?.length > 3 ? {key: 'name', parent: search} : {key: 'resources'}}
-                                    columns={columns}
-                                />}
+                                    /> :
+                                    <GridView
+                                        getCount={setCount}
+                                        url={URLS.classifier}
+                                        key={KEYS.classifier}
+                                        params={search && search?.length > 3 ? {
+                                            key: 'name',
+                                            parent: search
+                                        } : {key: 'resources'}}
+                                        columns={columns}
+                                    />}
                             </div>
                         </div>
                     </div>
