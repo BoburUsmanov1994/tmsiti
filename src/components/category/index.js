0171b0cfb23b1484afe840623f0d9c0c4819c0e9
BoburUsmanoff <bobur@pixeel.co.uk>
2023-04-30 21:59:43 +0500

    fix:lang

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Category = ({data,url='materials/volume',name='volume_name',logo_url='volume_logo'}) => {
     return (
-        <Link href={`/${url}/${get(data,'id')}`} className={'rounded-[5px] p-2.5 bg-white drop-shadow-category flex items-start min-h-full'}>
+        <Link href={`/${url}/${get(data,'id')}`} className={'rounded-[5px] transition-all p-2.5 bg-white drop-shadow-category flex items-start min-h-full border border-transparent hover:border-[#017EFA]'}>
             <Image height={38} width={38} src={get(data,logo_url)}  loader={() => get(data,logo_url)} />
             <h4 className={'ml-3 text-sm'}>{get(data,name)}</h4>
         </Link>
--- src/pages/500.js
+++ src/pages/500.js
@@ -3,17 +3,19 @@ import Main from "@/layouts/main";
 import Menu from "@/components/menu";
 import Section from "@/components/section";
 import Title from "@/components/title";
+import {useTranslation} from "react-i18next";
 
 
 const ErrorPage = () => {
+    const {t} = useTranslation()
     return (
         <Main>
             <Menu active={0}/>
             <Section>
                 <div className={'relative w-[75%] bg-white mx-auto my-28 p-24 drop-shadow-empty text-center'}>
                     <h1 className={'text-[#28366D] font-semibold mb-2.5 text-9xl'}>500</h1>
-                    <Title center>Kechirasiz, serverda xatolik</Title>
-                    <p>Serverning ichki xatoligi, iltimos keyinroq qayta urinib ko'ring!</p>
+                    <Title center>{t('title_500')}</Title>
+                    <p>{t('Serverning ichki xatoligi, iltimos keyinroq qayta urinib ko\'ring!')}</p>
                 </div>
             </Section>
         </Main>
