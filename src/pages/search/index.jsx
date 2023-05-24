f970b26229a388ec0ce186cb55abacc6d3f751ab
Bobur <boburusmanov1994@gmail.com>
2023-05-24 09:34:04 +0500

    fix:"search"

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
 import Main from "@/layouts/main";
 import Section from "@/components/section";
-import Menu, {menuData} from "@/components/menu";
+import Menu from "@/components/menu";
 import Title from "@/components/title";
 import Select from "@/components/select";
-import {debounce, get, head, isEqual, split} from "lodash";
+import {debounce, get, split} from "lodash";
 import useGetQuery from "@/hooks/api/useGetQuery";
 import {KEYS} from "@/constants/key";
 import {URLS} from "@/constants/url";
@@ -13,10 +13,11 @@ import Product from "@/components/product";
 import {useRouter} from "next/router";
 import {getDefaultValue, getOptionList} from "@/utils";
 import Pagination from "@/components/pagination";
+import {searchKeyList} from "@/constants";
 
 const Index = () => {
     const router = useRouter();
-    const {query,category} = router.query;
+    const {query = '', category = 'all'} = router.query;
     const [page, setPage] = useState(1);
     const {data, isLoading, isFetching} = useGetQuery({
         key: KEYS.search,
@@ -43,7 +44,7 @@ const Index = () => {
                     <div className="col-span-12 mb-5">
                         <label className={'block mb-1.5 text-[#202B57]'} htmlFor="#">Qidiruv maydoni</label>
                         <input defaultValue={query} onChange={debounce(function (e) {
-                                router.push(`/search?query=${e.target.value}`)
+                                router.push(`/search?query=${e.target.value}&category=${category}`)
                             }, 500
                         )}
                                className={'w-full h-11  rounded-[5px] outline-none px-3'} type="text"/>
@@ -51,21 +52,11 @@ const Index = () => {
                     <div className="col-span-12 mb-5">
                         <Select
                             name={'material'}
-                            defaultValue={getDefaultValue(getOptionList([{
-                                id: 11,
-                                title: 'all',
-                                url: '/',
-                                filterUrl: '/home'
-                            }, ...menuData], 'title', 'title', true), category)}
+                            defaultValue={getDefaultValue(getOptionList(searchKeyList, 'key', 'title'), category)}
                             getValue={(val) => {
                                 router.push(`/search?query=${query}&category=${get(val, 'value')}`)
                             }}
-                            options={getOptionList([{
-                                id: 11,
-                                title: 'all',
-                                url: '/',
-                                filterUrl: '/home'
-                            }, ...menuData], 'title', 'title', true)}
+                            options={getOptionList(searchKeyList, 'key', 'title')}
                             label={'Tanlangan mahsulot turi'}/>
                     </div>
 
@@ -75,13 +66,15 @@ const Index = () => {
                         <Title>Qidiruv natijalari</Title>
                     </div>
                     {
-                        get(data, 'data.results', []).map(material => <div
-                            key={get(material, 'resource_code')}
-                            className={'col-span-3 mb-[30px] '}>
-                            <Product viewUrl={get(split(get(material, 'resource_url'), '/'), '[1]')}
-                                     img={'resource_image'} code={'resource_code'}
-                                     name={'resource_name'} data={material}/>
-                        </div>)
+                        get(data, 'data.results', [])?.length == 0 ?
+                            <div className={'col-span-12 mb-5 text-[#515D89] text-xl'}>Sizning so’rovingizga mos natija
+                                topilmadi</div> : get(data, 'data.results', []).map(material => <div
+                                key={get(material, 'resource_code')}
+                                className={'col-span-3 mb-[30px] '}>
+                                <Product viewUrl={get(split(get(material, 'resource_url'), '/'), '[1]')}
+                                         img={'resource_image'} code={'resource_code'}
+                                         name={'resource_name'} data={material}/>
+                            </div>)
                     }
                     <div className={'col-span-12'}>
                         <Pagination page={page} setPage={setPage} pageCount={get(data, 'data.total_pages', 0)}/>
