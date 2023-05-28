552b04c9d184a3650c94cf0daecd1544a3586d07
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-28 13:07:09 +0500

    "next auth"

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -41,6 +41,6 @@ export const authOptions = {
     pages: {
         signIn: "/auth/login",
     },
-    secret:'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY='
+    secret: process.env.NEXTAUTH_SECRET || 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY='
 }
 export default NextAuth(authOptions)
