93d460894f78e23c5c32a16507139c7e93933136
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-28 18:26:10 +0500

    fix:"dashboard"

---
+++ src/pages/dashboard/materials/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Dashboard from "../../../layouts/dashboard";
+import Subheader from "../../../layouts/dashboard/components/subheader";
+
+const Materials = () => {
+    return (
+        <Dashboard>
+            <Subheader title={'Qurilish materiallari'} />
+            <div className="p-7">
+                dd
+            </div>
+        </Dashboard>
+    );
+};
+
+export default Materials;
