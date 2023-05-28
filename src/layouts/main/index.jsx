f311c484eebe53be9b46c92432e00e254a00d2d4
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-28 22:56:06 +0500

    fix:"materials api"

--- src/layouts/main/index.jsx
+++ src/layouts/main/index.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import Header from "@/components/header";
 import Wrapper from "@/components/wrapper";
 import Footer from "@/components/footer";
-import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
 
 const Main = ({children}) => {
-    const {data} = useQuery(['currency'],()=>axios.get('https://cbu.uz/uz/arkhiv-kursov-valyut/json'),{})
     return (
         <Wrapper>
             <Header/>
