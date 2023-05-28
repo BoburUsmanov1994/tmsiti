d5da5f28a5e773343ba4aff6045dcf3f985bdb3e
BoburUsmanoff <bobur@pixeel.co.uk>
2023-05-28 13:11:02 +0500

    "next auth"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    NEXTAUTH_URL: 'https://tmsiti.vercel.app',
+    NEXTAUTH_SECRET: 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=',
+  },
 }
 
 module.exports = nextConfig
