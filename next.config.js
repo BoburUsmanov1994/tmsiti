71aa5d7a31d0b4235346c867e946402553d026e2
Bobur <boburusmanov1994@gmail.com>
2023-06-22 17:54:38 +0500

    fix:"basepath"

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
     NEXTAUTH_URL: 'https://tmsiti.vercel.app',
     NEXTAUTH_SECRET: 'Ey7nTKnggBc0bRN8WUjyShw2qzOZ6KW4fUyqcKBePxY=',
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/classifier',
+        permanent: true,
+        basePath:false
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
