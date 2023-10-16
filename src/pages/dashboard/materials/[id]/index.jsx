013785bc8370fce6722c65d37545badcb70a0773
Khondamir <xondamir.maxmudov.01@mail.ru>
2023-10-16 17:37:17 +0500

    fixed

--- src/pages/dashboard/materials/[id]/index.jsx
+++ src/pages/dashboard/materials/[id]/index.jsx
@@ -94,7 +94,7 @@ const Index = () => {
     formData.append("material_measure", material_measure);
     editAdds(
       {
-        url: `${URLS.updateMaterial}/${id}`,
+        url: `${URLS.updateMaterial}${id}`,
         attributes: formData,
       },
       {
