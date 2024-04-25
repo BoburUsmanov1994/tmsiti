52751a16c8390499989415369b1fdd951e906c13
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-04-26 01:24:02 +0500

    average price material

--- src/pages/materials/[code]/index.jsx
+++ src/pages/materials/[code]/index.jsx
@@ -25,6 +25,9 @@ const ViewPage = () => {
   const [regionId, setRegionId] = useState(null);
   const [districtId, setDistrictId] = useState(null);
 
+
+
+
   const { data: currency } = useGetQuery({
     key: KEYS.currency,
     url: URLS.currency,
@@ -38,6 +41,14 @@ const ViewPage = () => {
     url: `${URLS.materials}${code}/`,
     enabled: !!code,
   });
+
+  const {data: materialAds, isLoading: isLoadingMaterialAds} = useGetQuery({
+    key: KEYS.materialAds,
+    url: `${URLS.materialAds}${code}/`,
+
+  })
+
+
   const { data: price } = useGetQuery({
     key: [KEYS.prices, code],
     url: `${URLS.prices}?resource=${code}`,
@@ -59,6 +70,10 @@ const ViewPage = () => {
     },
     enable: !!regionId,
   });
+
+  const totalPrice = get(materialAds, "data.results", []).reduce((sumResult, price) => sumResult + price["material_price"], 0)
+
+
 
   // const { data: gost, isLoading: isLoadingGost } = useGetQuery({
   //   key: KEYS.materialGost,
@@ -239,26 +254,27 @@ const ViewPage = () => {
         <Menu active={1} />
         <Section className={"!bg-white"}>
           <div className="grid grid-cols-12 tablet:gap-x-10 gap-x-4">
-            <div className="tablet:col-span-5 col-span-12  items-center flex justify-center tablet:items-start tablet:justify-start relative h-64">
+            <div
+                className="tablet:col-span-5 col-span-12  items-center flex justify-center tablet:items-start tablet:justify-start relative h-64">
               {get(material, "data.material_image") ? (
-                <Image
-                  className={"mr-2"}
-                  layout={"fill"}
-                  objectFit={"contain"}
-                  loader={() => get(material, "data.material_image")}
-                  src={get(material, "data.material_image")}
-                  alt={"code"}
-                />
+                  <Image
+                      className={"mr-2"}
+                      layout={"fill"}
+                      objectFit={"contain"}
+                      loader={() => get(material, "data.material_image")}
+                      src={get(material, "data.material_image")}
+                      alt={"code"}
+                  />
               ) : (
-                <Image
-                  className={
-                    " laptop:w-[370px] laptop:h-[260px] tablet:w-[330px] tablet:h-[220px] w-[300px] h-[200px] "
-                  }
-                  width={370}
-                  height={260}
-                  src={"/images/material.png"}
-                  alt={"company"}
-                />
+                  <Image
+                      className={
+                        " laptop:w-[370px] laptop:h-[260px] tablet:w-[330px] tablet:h-[220px] w-[300px] h-[200px] "
+                      }
+                      width={370}
+                      height={260}
+                      src={"/images/material.png"}
+                      alt={"company"}
+                  />
               )}
             </div>
             <div className="tablet:col-span-7 col-span-12">
@@ -267,18 +283,18 @@ const ViewPage = () => {
                   {/*material kodi*/}
                   <div className={"inline-flex mr-8"}>
                     <Image
-                      className={
-                        "mr-2 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
-                      }
-                      width={24}
-                      height={24}
-                      src={"/icons/code.svg"}
-                      alt={"code"}
+                        className={
+                          "mr-2 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
+                        }
+                        width={24}
+                        height={24}
+                        src={"/icons/code.svg"}
+                        alt={"code"}
                     />
                     <span
-                      className={
-                        "font-medium tablet:text-sm laptop:text-base text-xs"
-                      }
+                        className={
+                          "font-medium tablet:text-sm laptop:text-base text-xs"
+                        }
                     >
                       #{get(material, "data.material_csr_code")}
                     </span>
@@ -286,18 +302,18 @@ const ViewPage = () => {
                   {/*ko'rilganlar soni*/}
                   <div className={"inline-flex mr-8"}>
                     <Image
-                      className={
-                        "mr-2 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
-                      }
-                      width={24}
-                      height={24}
-                      src={"/icons/eye.svg"}
-                      alt={"code"}
+                        className={
+                          "mr-2 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
+                        }
+                        width={24}
+                        height={24}
+                        src={"/icons/eye.svg"}
+                        alt={"code"}
                     />
                     <span
-                      className={
-                        "font-medium tablet:text-sm laptop:text-base text-xs"
-                      }
+                        className={
+                          "font-medium tablet:text-sm laptop:text-base text-xs"
+                        }
                     >
                       {get(material, "data.material_views_count", 0)}
                     </span>
@@ -305,18 +321,18 @@ const ViewPage = () => {
                   {/*saqlash*/}
                   <div className={"inline-flex mr-8 cursor-pointer"}>
                     <Image
-                      className={
-                        "mr-1.5 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
-                      }
-                      width={20}
-                      height={20}
-                      src={"/icons/stick.svg"}
-                      alt={"code"}
+                        className={
+                          "mr-1.5 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
+                        }
+                        width={20}
+                        height={20}
+                        src={"/icons/stick.svg"}
+                        alt={"code"}
                     />
                     <span
-                      className={
-                        "font-medium tablet:text-sm laptop:text-base text-xs"
-                      }
+                        className={
+                          "font-medium tablet:text-sm laptop:text-base text-xs"
+                        }
                     >
                       {t("save")}
                     </span>
@@ -324,36 +340,36 @@ const ViewPage = () => {
                 </div>
                 {/*gost nomeri*/}
                 <div
-                  className={
-                    "inline-flex  cursor-pointer border p-2 rounded-[8px]"
-                  }
+                    className={
+                      "inline-flex  cursor-pointer border p-2 rounded-[8px]"
+                    }
                 >
                   <Image
-                    className={
-                      "mr-1.5 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
-                    }
-                    width={24}
-                    height={24}
-                    src={"/icons/document_icon.svg"}
-                    alt={"code"}
+                      className={
+                        "mr-1.5 tablet:w-[20px] tablet:h-[20px] laptop:w-[24px] laptop:h-[24px] w-[18px] h-[18px]"
+                      }
+                      width={24}
+                      height={24}
+                      src={"/icons/document_icon.svg"}
+                      alt={"code"}
                   />
                   {isEmpty(get(material, "data.materil_gost")) ? (
-                    <span
-                      className={
-                        "font-medium tablet:text-sm laptop:text-base text-xs"
-                      }
-                    >
+                      <span
+                          className={
+                            "font-medium tablet:text-sm laptop:text-base text-xs"
+                          }
+                      >
                       {get(material, "data.materil_gost")}
                     </span>
                   ) : (
-                    <span>Ma'lumot mavjud emas</span>
+                      <span>Ma'lumot mavjud emas</span>
                   )}
                 </div>
               </div>
               <h2
-                className={
-                  "my-3 laptop:text-xl tablet:tex-lg text-base tablet:text-start  text-center  font-semibold"
-                }
+                  className={
+                    "my-3 laptop:text-xl tablet:tex-lg text-base tablet:text-start  text-center  font-semibold"
+                  }
               >
                 {get(material, "data.material_name")}
               </h2>
@@ -361,16 +377,48 @@ const ViewPage = () => {
                 {get(material, "data.material_desc", "")}
               </p>
             </div>
+
+          </div>
+
+          <div className={"grid-cols-12 grid mt-[20px] gap-x-4 "}>
+            <div className={"col-span-4 p-[20px] shadow-2xl rounded-[4px]"}>
+              <h4 className={"mb-[8px] text-[#22497C] font-bold"}>Davlat soliq qo'mitasi</h4>
+              <div className={"flex justify-between"}>
+                <p className={"text-sm mb-[5px]"}>O'tgan oydagi savdolar soni:</p>
+                <span className={"text-sm"}>20</span>
+              </div>
+
+              <div className={"flex justify-between"}>
+                <p className={"text-sm mb-[5px]"}>O'rtacha narx:</p>
+                <span className={"text-sm"}>20</span>
+              </div>
+            </div>
+            <div className={"col-span-4 p-[20px] shadow-2xl rounded-[4px]"}>
+              <h4 className={"mb-[8px] text-[#22497C] font-bold"}>Tovar xom-ashyo birjasi</h4>
+              <div className={"flex justify-between"}>
+                <p className={"text-sm mb-[5px]"}>O'tgan oydagi savdolar hajmi(4 hafta):</p>
+                <span className={"text-sm"}>20</span>
+              </div>
+
+              <div className={"flex justify-between"}>
+                <p className={"text-sm mb-[5px]"}>O'rtacha narx:</p>
+                <span className={"text-sm"}>20</span>
+              </div>
+            </div>
+            <div className={"col-span-4 p-[20px] shadow-2xl rounded-[4px]"}>
+              <h4 className={"mb-[8px] text-[#22497C] font-bold"}>O'rtacha narx:</h4>
+              {(totalPrice / get(materialAds, "data.results", []).length).toFixed(3)}
+            </div>
           </div>
         </Section>
         <Section>
           <div className="grid grid-cols-12">
             <div className="col-span-12">
               <GridView
-                HeaderBody={
-                  <div className="flex tablet:flex-row  flex-col mb-5">
-                    <Select
-                      getValue={(val) => setRegionId(get(val, "value"))}
+                  HeaderBody={
+                    <div className="flex tablet:flex-row  flex-col mb-5">
+                      <Select
+                          getValue={(val) => setRegionId(get(val, "value"))}
                       sm
                       label={t("region")}
                       options={getOptionList(
