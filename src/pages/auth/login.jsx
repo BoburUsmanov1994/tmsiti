5827716561abd2692af00116d8d66f3c5b31ec69
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-05-28 12:41:26 +0500

    fixed

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
                 onSuccess: ({data}) => {
                     setToken(get(data, 'token'))
                     toast.success('We have sent confirmation code to your email address', {position: 'top-right'})
-                    router.push("/dashboard")
+                    router.push("/dashboard/customer/my-orders")
 
                 }
             })
