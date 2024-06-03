b7e763b6d159eb94bfcef6d3997ab16bda8759e7
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-06-04 02:06:59 +0500

    fixed

--- src/pages/dashboard/orders/index.jsx
+++ src/pages/dashboard/orders/index.jsx
@@ -68,7 +68,7 @@ const Index = () => {
             render: ({ row }) =>
                 get(row, "order_status") === "new_order" ?
                     <div className={"flex flex-col gap-y-2"}>
-                            <button onClick={() => handleSendOrderStatus(get(row, "id"))} className={"bg-green-600 hover:bg-green-700 active:bg-green-500 text-white py-2 px-8 rounded-[6px]"}>
+                            <button onClick={handleSendOrderStatus(get(row, "id"))} className={"bg-green-600 hover:bg-green-700 active:bg-green-500 text-white py-2 px-8 rounded-[6px]"}>
                                 Qabul qilish
                             </button>
                             <button className={"bg-red-600 hover:bg-red-700 active:bg-red-500 text-white py-2 px-8 rounded-[6px]"}>
