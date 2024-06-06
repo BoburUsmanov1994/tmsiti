4c788a11c257cd700a2594250c6b90afa9f52812
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-06-06 07:31:08 +0500

    fixed comment

--- src/pages/dashboard/customer/my-orders/index.jsx
+++ src/pages/dashboard/customer/my-orders/index.jsx
@@ -208,7 +208,7 @@ const Index = () => {
             </div>
             {isOpen &&
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-                  <div className="bg-white p-8 rounded shadow-md w-[700px] h-auto flex flex-col">
+                  <div className="bg-white p-8 rounded shadow-md w-[1000px] h-auto flex flex-col">
                       <Image onClick={closeModal} src={"/icons/closeModal.svg"} alt={"close"} width={30} height={30}/>
                       <h1 ref={ratingRef}>5</h1>
                       <textarea ref={commentRef} rows={10} placeholder={"Izoh qoldirish"}>
