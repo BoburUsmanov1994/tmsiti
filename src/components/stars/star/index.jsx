8e95a1368025e2a6fa91cac4bcca79e7619040d0
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-06-06 07:52:47 +0500

    fixed comment

--- src/components/stars/star/index.jsx
+++ src/components/stars/star/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Star = ({ selected = false, onClick }) => (
-    <span onClick={onClick} style={{ cursor: 'pointer', color: selected ? '#ffd700' : '#ccc', fontSize: "25px" }}>
+    <span onClick={onClick} style={{ cursor: 'pointer', color: selected ? '#ffd700' : '#ccc', fontSize: "50px" }}>
     &#9733;
   </span>
 );
