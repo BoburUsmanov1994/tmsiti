da6723bbdb49e25830310e818e921d80db5b7267
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-06-08 18:29:06 +0500

    fixed

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AuthLayout from "../../layouts/auth";
 import {useForm} from "react-hook-form";
 import usePostQuery from "../../hooks/api/usePostQuery";
