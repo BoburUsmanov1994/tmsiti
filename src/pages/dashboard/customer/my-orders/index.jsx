8626995f82e1dcf91b0a8c7fbde6de13575f3c32
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-06-11 23:20:08 +0500

    fix:some

--- src/pages/dashboard/customer/my-orders/index.jsx
+++ src/pages/dashboard/customer/my-orders/index.jsx
@@ -319,7 +319,7 @@ const Index = () => {
 
                             <button
                                 className={"bg-blue-500 hover:bg-blue-600 active:bg-blue-400 mt-[30px] text-white w-full text-lg py-2 rounded-[6px]"}
-                                onClick={() => handleSendComment({isOpen})}>Yuborish
+                                onClick={() => handleSendComment(isOpen)}>Yuborish
                             </button>
                         </div>
                     </div>
