54f3f307e617ad1d6f80e648289a44e57320f145
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-07-04 10:46:37 +0500

    API_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-    API_URL: 'https://jetmind.uz/',
+    API_URL: 'http://185.100.54.70/',
     FILE_URL: 'https://backend-market.tmsiti.uz/static/categories/materials/',
     DEFAULT_APP_LANG: 'uz'
 };
