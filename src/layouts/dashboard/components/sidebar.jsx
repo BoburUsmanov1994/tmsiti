dce788efcf1440b0f91c245e2289d7701df86f34
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-07-15 03:33:52 +0500

    fixed

--- src/layouts/dashboard/components/sidebar.jsx
+++ src/layouts/dashboard/components/sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Brand from "../../../components/brand";
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
