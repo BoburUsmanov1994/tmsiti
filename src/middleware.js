88616c22259a54f98c1411b6e72c3939822eb3d7
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-09-25 15:26:12 +0500

    fixed

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,10 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/selected", "/cart"],
+  matcher: [
+    "/dashboard/:path*",
+    "/dashboard/customer/:path*",
+    "/selected",
+    "/cart",
+  ],
 };
