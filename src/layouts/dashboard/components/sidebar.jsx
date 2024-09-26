9bf888aeb11e66f2c6b725bc72ee7d4a4bf1f197
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-09-26 08:39:05 +0500

    fixed

--- src/layouts/dashboard/components/sidebar.jsx
+++ src/layouts/dashboard/components/sidebar.jsx
@@ -11,6 +11,52 @@ import { KEYS } from "@/constants/key";
 import { URLS } from "@/constants/url";
 import { useSession } from "next-auth/react";
 
+const customerMenu = [
+  {
+    id: 1,
+    title: "Mening buyurtmalarim",
+    url: "/dashboard/customer/my-orders",
+  },
+];
+
+const companyMenu = [
+  {
+    id: 1,
+    title: "Bosh sahifa",
+    url: "/dashboard",
+  },
+  {
+    id: 2,
+    title: "Materiallar va buyumlar",
+    url: "/dashboard/materials",
+  },
+  {
+    id: 3,
+    title: "Mashina mexanizmlar",
+    url: "/dashboard/machine-mechano",
+  },
+  {
+    id: 4,
+    title: "Qurilish ishlari",
+    url: "/dashboard/works",
+  },
+  {
+    id: 5,
+    title: "Kichik mexanizatsiya",
+    url: "/dashboard/small-mechano",
+  },
+  {
+    id: 6,
+    title: "Kompaniya haqida",
+    url: "/dashboard/about",
+  },
+  {
+    id: 7,
+    title: "Buyurtmalar",
+    url: "/dashboard/orders",
+  },
+];
+
 const Sidebar = ({ openSidebar }) => {
   const { data: session } = useSession();
   const { t } = useTranslation();
@@ -73,17 +119,20 @@ const Sidebar = ({ openSidebar }) => {
       </div>
       {get(session, "user.role") === "company" ? (
         <ul className={"text-[#8D97AD] mt-3"}>
-          <li>
-            <Link
-              className={clsx("py-3.5 px-7 block hover:text-white", {
-                "bg-[#3F54A5] text-white": router.pathname === "/dashboard",
-              })}
-              href={"/dashboard"}
-            >
-              {t("Bosh sahifa")}
-            </Link>
-          </li>
-          <li>
+          {companyMenu.map((item) => (
+            <li key={get(item, "id")}>
+              <Link
+                className={clsx("py-3.5 px-7 block hover:text-white", {
+                  "bg-[#3F54A5] text-white":
+                    router.pathname === `${get(item, "url")}`,
+                })}
+                href={`${get(item, "url")}`}
+              >
+                {t(`${get(item, "title")}`)}
+              </Link>
+            </li>
+          ))}
+          {/* <li>
             <Link
               className={clsx(
                 "py-3.5 px-7 block hover:text-white",
@@ -183,9 +232,9 @@ const Sidebar = ({ openSidebar }) => {
             >
               {t("Kompaniya haqida")}
             </Link>
-          </li>
+          </li> */}
 
-          <li>
+          {/* <li>
             <Link
               className={clsx(
                 "py-3.5 px-7 block hover:text-white",
@@ -202,155 +251,26 @@ const Sidebar = ({ openSidebar }) => {
             >
               {t("Buyurtmalar")}
             </Link>
-          </li>
+          </li> */}
         </ul>
       ) : get(session, "user.role") === "customer" ? (
         <ul className={"text-[#8D97AD] mt-3"}>
-          <li>
-            <Link
-              className={clsx("py-3.5 px-7 block hover:text-white", {
-                "bg-[#3F54A5] text-white":
-                  router.pathname === "/dashboard/customer/my-orders",
-              })}
-              href={"/dashboard/customer/my-orders"}
-            >
-              Mening buyurtmalarim
-            </Link>
-          </li>
+          {customerMenu.map((item) => (
+            <li key={get(item, "id")}>
+              <Link
+                className={clsx("py-3.5 px-7 block hover:text-white", {
+                  "bg-[#3F54A5] text-white":
+                    router.pathname === `${get(item, "url")}`,
+                })}
+                href={`${get(item, "url")}`}
+              >
+                Mening buyurtmalarim
+              </Link>
+            </li>
+          ))}
         </ul>
       ) : (
-        <ul className={"text-[#8D97AD] mt-3"}>
-          <li>
-            <Link
-              className={clsx("py-3.5 px-7 block hover:text-white", {
-                "bg-[#3F54A5] text-white": router.pathname === "/dashboard",
-              })}
-              href={"/dashboard"}
-            >
-              {t("Bosh sahifa")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/materials",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/materials/add-ads",
-                }
-              )}
-              href={"/dashboard/materials"}
-            >
-              {t("Materiallar va buyumlar")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/machine-mechano",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/machine-mechano/add-ads",
-                }
-              )}
-              href={"/dashboard/machine-mechano"}
-            >
-              {t("Mashina mexanizmlar")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/works",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/works/add-ads",
-                }
-              )}
-              href={"/dashboard/works"}
-            >
-              {t("Qurilish ishlari")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/small-mechano",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/small-mechano/add-ads",
-                }
-              )}
-              href={"/dashboard/small-mechano"}
-            >
-              {t("Kichik mexanizatsiya")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/technos",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/technos/add-ads",
-                }
-              )}
-              href={"/dashboard/technos"}
-            >
-              {t("Uskuna va qurilmalar")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={clsx("py-3.5 px-7 block hover:text-white", {
-                "bg-[#3F54A5] text-white":
-                  router.pathname === "/dashboard/about",
-              })}
-              href={"/dashboard/about"}
-            >
-              {t("Kompaniya haqida")}
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className={clsx(
-                "py-3.5 px-7 block hover:text-white",
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/orders",
-                },
-                {
-                  "bg-[#3F54A5] text-white":
-                    router.pathname === "/dashboard/orders/add-ads",
-                }
-              )}
-              href={"/dashboard/orders"}
-            >
-              {t("Buyurtmalar")}
-            </Link>
-          </li>
-        </ul>
+        ""
       )}
     </div>
   );
